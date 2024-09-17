3e2fffef57a791cd73a978036d7999e3e737c53a
Mmanare Mahlatse nkoana <mahlatsemmanarenkoana@gmail.com>
2024-09-17 16:51:56 +0200

    routing quiz

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,37 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def update
+    @task = Task.new(params[:task])
+    @task.update(params[:task])
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
